673995136ebc29d13ae0c9d9570662e9410a3403
Asitha Senanayake <asitha_sena@yahoo.com>
2020-12-12 20:39:21 -0500

    Test files updated Test files updated so that 'pytest' can be called from the repo root directory instead of repo_root/tests directory. Appending to sys.path no longer required. This is expected to allow Github Actions CI to run without errors by calling 'python -m pytest'.

--- tests/test_ags4.py
+++ tests/test_ags4.py
@@ -1,32 +1,31 @@
-import sys
+# import sys
 # Prepend path so that AGS4.py is loaded from project file
 # instead of current installation
-sys.path.insert(0, '../')
-
+# sys.path.insert(0, './')
 from python_ags4 import AGS4, __version__
 import toml
 
 
 def test_version():
-    pyproject = toml.load('../pyproject.toml')
+    pyproject = toml.load('pyproject.toml')
 
     assert __version__ == pyproject['tool']['poetry']['version']
 
 
 def test_AGS4_to_dict():
-    tables, headings = AGS4.AGS4_to_dict('test_data.ags')
+    tables, headings = AGS4.AGS4_to_dict('tests/test_data.ags')
 
     assert tables['PROJ']['PROJ_ID'][2] == '123456'
 
 
 def test_AGS4_to_dataframe():
-    tables, headings = AGS4.AGS4_to_dataframe('test_data.ags')
+    tables, headings = AGS4.AGS4_to_dataframe('tests/test_data.ags')
 
     assert tables['LOCA'].loc[2, 'LOCA_ID'] == 'Location_1'
 
 
 def test_convert_to_numeric():
-    tables, headings = AGS4.AGS4_to_dataframe('test_data.ags')
+    tables, headings = AGS4.AGS4_to_dataframe('tests/test_data.ags')
     LOCA = AGS4.convert_to_numeric(tables['LOCA'])
 
     assert LOCA.loc[0, 'LOCA_NATE'] == 100000.01
@@ -35,17 +34,17 @@ def test_convert_to_numeric():
 
 
 def test_dataframe_to_AGS4():
-    tables, headings = AGS4.AGS4_to_dataframe('test_data.ags')
+    tables, headings = AGS4.AGS4_to_dataframe('tests/test_data.ags')
 
     AGS4.dataframe_to_AGS4(tables, headings, 'test.out')
     AGS4.dataframe_to_AGS4(tables, {}, 'test.out')
 
 
 def test_convert_to_text():
-    tables, headings = AGS4.AGS4_to_dataframe('test_data.ags')
+    tables, headings = AGS4.AGS4_to_dataframe('tests/test_data.ags')
     LOCA_num = AGS4.convert_to_numeric(tables['LOCA'])
 
-    LOCA_txt = AGS4.convert_to_text(LOCA_num, 'DICT.ags')
+    LOCA_txt = AGS4.convert_to_text(LOCA_num, 'tests/DICT.ags')
 
     assert LOCA_txt.loc[0, 'LOCA_NATE'] == "100000.01"
     assert LOCA_txt.loc[2, 'LOCA_NATN'] == "5000000.20"
--- tests/test_python_ags4.py
+++ tests/test_python_ags4.py
@@ -1,10 +1,8 @@
 import unittest
-
-import sys
+# import sys
 # Prepend path so that AGS4.py is loaded from project file
 # instead of current installation
-sys.path.insert(0, '../')
-
+# sys.path.insert(0, './')
 from python_ags4 import AGS4
 
 
@@ -17,17 +15,17 @@ class Test_AGS4(unittest.TestCase):
         pass
 
     def test_AGS4_to_dict(self):
-        data, headings = AGS4.AGS4_to_dict('test_data.ags')
+        data, headings = AGS4.AGS4_to_dict('tests/test_data.ags')
 
         self.assertEqual(data['PROJ']['PROJ_ID'][2], '123456')
 
     def test_AGS4_to_dataframe(self):
-        data, headings = AGS4.AGS4_to_dataframe('test_data.ags')
+        data, headings = AGS4.AGS4_to_dataframe('tests/test_data.ags')
 
         self.assertEqual(data['LOCA'].loc[2, 'LOCA_ID'], 'Location_1')
 
     def test_convert_to_numeric(self):
-        data, headings = AGS4.AGS4_to_dataframe('test_data.ags')
+        data, headings = AGS4.AGS4_to_dataframe('tests/test_data.ags')
         LOCA = AGS4.convert_to_numeric(data['LOCA'])
 
         self.assertEqual(LOCA.loc[0, 'LOCA_NATE'], 100000.01)
