3d33a846eade25f75ba9f1e02ce300221331799e
Asitha Senanayake <asitha_sena@yahoo.com>
2021-01-13 23:25:07 -0500

    Added a 'check' sub-command to the cli. It can call AGS4.check_file() function to check the input file for AGS4 errors.

--- python_ags4/ags4_cli.py
+++ python_ags4/ags4_cli.py
@@ -3,9 +3,14 @@
 import click
 from python_ags4 import AGS4
 from rich.console import Console
+import pkg_resources
+import os
 
 # Create rich console for pretty printing
 console = Console()
+
+# Get path to standard AGS4 dictionary
+path_to_standard_dictionary = pkg_resources.resource_filename('python_ags4', 'Standard_dictionary_v4_1.ags')
 
 
 @click.group()
@@ -74,6 +79,86 @@ def convert(input_file, output_file, format_columns, dictionary):
     else:
         console.print('[red]ERROR: Invalid filenames.[/red]')
         console.print('[red]Try "ags4_cli convert --help" to see help and examples.[/red]')
+
+
+
+@main.command()
+@click.argument('input_file', type=click.Path(exists=True))
+@click.option('-o', '--output_file', type=click.Path(writable=True), default=None,
+              help="Path to save error log")
+@click.option('-d', '--dictionary', type=click.Path(exists=True), default=path_to_standard_dictionary,
+              help="Path to AGS4 dictionary file.")
+def check(input_file, dictionary, output_file):
+    '''Check .ags file for error based AGS4 rules.
+
+    INPUT_FILE   Path to .ags to be checked
+    '''
+
+    if input_file.endswith('.ags'):
+        console.print(f'[green]Opening file... [bold]{input_file}[/bold][/green]')
+        console.print('')
+
+        with console.status("[bold green]Checking file...") as status:
+            ags_errors = AGS4.check_file(input_file, standard_AGS4_dictionary=dictionary)
+
+        # Dictionay evaluates to False if empty
+        if bool(ags_errors) is False:
+            console.print('\n[green]File check complete! No errors found. :heavy_check_mark:[/green]\n')
+
+        else:
+            # Count number of entries in error log
+            error_count = 0
+            for key, val in ags_errors.items():
+                error_count += len(val)
+
+            # Print errors to screen if list is short enough.
+            if error_count < 100:
+                print_to_screen(ags_errors)
+
+                console.print(f'\n[yellow]File check complete! {error_count} errors found![/yellow]')
+
+                if output_file is not None:
+                    save_to_file(output_file, ags_errors, input_file, error_count)
+                    console.print(f'\n[yellow]Error report saved in {output_file}.[/yellow]\n')
+
+            else:
+                console.print(f'\n[yellow]File check complete! {error_count} errors found![/yellow]')
+                console.print('\n[yellow]Error report too long to print to screen.[/yellow]')
+
+                if output_file is None:
+                    output_dir = os.path.dirname(input_file)
+                    output_file = os.path.join(output_dir, 'error_log.txt')
+
+                save_to_file(output_file, ags_errors, input_file, error_count)
+
+                console.print(f'\n[yellow]Error report saved in {output_file}[/yellow]\n')
+
+    else:
+        console.print('[red]ERROR: Only .ags files are accepted as input.[/red]')
+
+
+def print_to_screen(ags_errors):
+    '''Print error report to screen.'''
+    for key in ags_errors:
+        console.print(f'''[underline]{key}[/underline]:''')
+        for entry in ags_errors[key]:
+            console.print(f'''  Line {entry['line']}\t [bold]{entry['group'].strip('"')}[/bold]\t {entry['desc']}''')
+        console.print('')
+
+
+def save_to_file(output_file, ags_errors, input_file, error_count):
+    '''Save error report to file.'''
+
+    with open(output_file, 'w') as f:
+        f.write(f'Input file: {input_file}\r\n')
+        f.write(f'{error_count} errors found!\r\n')
+        f.write('\r\n')
+
+        for key in ags_errors:
+            f.write(f'{key}:\r\n')
+            for entry in ags_errors[key]:
+                f.write(f'''  Line {entry['line']}\t {entry['group'].strip('"')}\t {entry['desc']}\r\n''')
+            f.write('\r\n')
 
 
 if __name__ == '__main__':
