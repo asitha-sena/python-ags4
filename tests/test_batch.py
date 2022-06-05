e8b649fd38aa58a707edc7d7bf1175f44d23a9c7
Asitha Senanayake <asitha_sena@yahoo.com>
2022-06-05 12:14:52 -0400

    Update test_batch.py to catch exceptions
    This modification is necessary after parts of AGS4.py and check.py were
    changed to raise AGS4Error exceptions instead of silently failing by
    calling sys.exit(). These exceptions cause unit testing to fail if not
    properly captured.

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -11,25 +11,32 @@ def checkandcompare(AGSFilename):
     ErrorFilename = AGSFilename.replace(".ags", ".errors")
     CheckFilename = AGSFilename.replace(".ags", ".check")
 
-    error_list = AGS4.check_file(AGSFilename, standard_AGS4_dictionary='python_ags4/Standard_dictionary_v4_1.ags')
+    try:
+        error_list = AGS4.check_file(AGSFilename, standard_AGS4_dictionary='python_ags4/Standard_dictionary_v4_1.ags')
 
-    # remove Metadata item it is exists as this will change everytime it is checked
-    if "Metadata" in error_list.keys():
-        error_list.pop("Metadata")
+        # remove Metadata item it is exists as this will change everytime it is checked
+        if "Metadata" in error_list.keys():
+            error_list.pop("Metadata")
 
-    f = open(ErrorFilename, "w")
-    app_json = json.dumps(error_list)
-    f.write(app_json)
-    f.close()
+        f = open(ErrorFilename, "w")
+        app_json = json.dumps(error_list)
+        f.write(app_json)
+        f.close()
 
-    if glob.glob(CheckFilename):
-        if filecmp.cmp(ErrorFilename, CheckFilename, shallow=False):
-            os.remove(ErrorFilename)  # remove the error file as this is no longer required
-            return 'Passed - ' + AGSFilename
+    except AGS4.AGS4Error as err:
+        f = open(ErrorFilename, "w")
+        f.write(str(err))
+        f.close()
+
+    finally:
+        if glob.glob(CheckFilename):
+            if filecmp.cmp(ErrorFilename, CheckFilename, shallow=False):
+                os.remove(ErrorFilename)  # remove the error file as this is no longer required
+                return 'Passed - ' + AGSFilename
+            else:
+                return 'Failed - ' + AGSFilename + ' - errors different from file'
         else:
-            return 'Failed - ' + AGSFilename + ' - errors different from file'
-    else:
-        return 'Failed - ' + AGSFilename + ' - no file to check against'
+            return 'Failed - ' + AGSFilename + ' - no file to check against'
 
 
 # Routine to check all the AGS files within the Test_files directory and compare the results against a known output file
