ca258e4eed896b48c889eef0018a34529ced9a9a
Asitha Senanayake <asitha_sena@yahoo.com>
2025-10-20 15:25:26 -0500

    fixup! fix: DeprecationWarning for datetime.datetime.utcnow()
    Tests failed because 'datetime.UTC' does not exist in Python 3.9 and
    3.11. Therefore had to use 'datetime.timezone.utc' instead.

--- python_ags4/check.py
+++ python_ags4/check.py
@@ -259,7 +259,7 @@ def add_meta_data(filepath_or_buffer, standard_dictionary, ags_errors={}, encodi
     if standard_dictionary is not None:
         add_error_msg(ags_errors, 'Metadata', 'Dictionary', '', f'{os.path.basename(standard_dictionary)}')
 
-    add_error_msg(ags_errors, 'Metadata', 'Time (UTC)', '', f'{datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d %H:%M:%S")}')
+    add_error_msg(ags_errors, 'Metadata', 'Time (UTC)', '', f'{datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d %H:%M:%S")}')
 
     add_error_msg(ags_errors, 'Metadata', 'File encoding', '', encoding)
 
